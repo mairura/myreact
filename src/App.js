0a60c3c5477c1ca1fa3b1ca80ff393b739195b07
unknown <mairurated@gmail.com>
2022-04-05 08:54:01 +0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
+
 function App() {
-  return <div>Hellow!</div>;
+  return (
+    <div>
+      <h2>My Todo List</h2>
+      <Todo text="React Begginer course" />
+      <Todo text="React Begginer course Day 2" />
+      <Todo text="React Begginer course Last Day" />
+      <Modal />
+      <Backdrop />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Backdrop.js
@@ -0,0 +1,5 @@
+function Backdrop() {
+  return <div className="Backdrop"></div>;
+}
+
+export default Backdrop;
