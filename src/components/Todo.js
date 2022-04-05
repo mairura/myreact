42136c517399725b2dd9f0874d81dc11a1522c95
unknown <mairurated@gmail.com>
2022-04-05 12:47:27 +0300

    finished first components in react

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false); //
+
   function deleteHandler() {
-    console.log("Clicked!");
-    console.log(props.text);
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
 
   return (
@@ -12,6 +21,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
